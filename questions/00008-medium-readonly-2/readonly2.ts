d34ca85f4813ed1d73d51fc7facd4bc532b6c81a
yangyi <iyoung2018@126.com>
2022-06-22 20:54:09 +0800

    6.22

--- questions/00008-medium-readonly-2/readonly2.ts
+++ questions/00008-medium-readonly-2/readonly2.ts
@@ -1,2 +1,4 @@
-export type MyReadonly2<T, K extends keyof T = keyof T> = T & { readonly [P in K]: T[P] }
+export type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & { readonly [P in K]: T[P] }
 // 操作符&，使用后获得交叉类型
+export type MyReadonly2_1<T, K extends keyof T = keyof T> = T & { readonly [P in K]: T[P] }
+// 上面的写法会报错，说是4.5版本的一个bug，{readonly a: string} & {a: string}与{a: string}从概念上是一样的，后面的会覆写&符号前面的类型。
